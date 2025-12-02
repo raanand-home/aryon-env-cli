7c79c0c96d93f8b58ab910e3421f91ab5d5f19aa
Raanan Denis <rdenis@paloaltonetworks.com>
2025-12-02 11:16:27 +0200

    wip

--- src/env_cli/main.py
+++ src/env_cli/main.py
@@ -14,6 +14,10 @@ class EnvsManager():
     def push_env_data(self,config:EnvConfig):
         config_yaml = yaml.safe_dump(config.model_dump())
         self._bucket.put_object_data(config.name,config_yaml)
+
+    def get_env(self, name):
+        yaml_content = self._bucket.get_object_data(name)
+        return EnvConfig(**yaml.safe_load(yaml_content))
     
 
 _env = EnvsManager()
@@ -35,6 +39,16 @@ def new_env(git_repo,name):
         return -1
     _env.push_env_data(EnvConfig(name=name, git_repo=git_repo))
 
+@cli.command()
+@click.argument("name")
+def get(name):
+    print(yaml.dump(_env.get_env(name).model_dump()))
+    
+
+@cli.command()
+@click.argument("name")
+def checkout_ref(name):
+    print(_env.get_env(name).follow_branch)
 
 
 if __name__ == '__main__':
